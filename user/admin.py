80d7344f4afedc5820ee6fa6755ce738d50b47ff
deecokeson <cokeroyindamola@gmail.com>
2024-07-19 16:00:08 +0100

    Initial commit

---
+++ user/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import CustomUser
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+  list_display = ('username', 'id', 'email',)
+  search_fields = ('username', 'email',)
+
+# Register your models here.
